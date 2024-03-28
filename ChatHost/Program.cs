85365252d5a7900a75a01aea0322adfb34879bcd
kirillkhlopkoff <114176969+kirillkhlopkoff@users.noreply.github.com>
2024-03-28 18:36:42 +0200

    Добавьте файлы проекта.

---
+++ ChatHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatHost
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var host = new ServiceHost(typeof(WCF_Chat.ServiceChat)))
+            {
+                host.Open();
+                Console.WriteLine("Хост стартовал!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ WCF_Chat/ServiceChat.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Chat
+{
+    [ServiceBehavior(InstanceContextMode= InstanceContextMode.Single)]
+    public class ServiceChat : IServiceChat
+    {
+        List<ServerUser> users = new List<ServerUser>();
+        int nextId = 1;
+        public int Connect(string name)
+        {
+            ServerUser user = new ServerUser() { 
+                ID = nextId,
+                Name = name,
+                operationContext = OperationContext.Current
+            };
+            nextId++;
+
+            SendMessage(user.Name + " подключился к чату!", 0);
+            users.Add(user);
+            return user.ID;
+        }
+
+        public void DisConnect(int id)
+        {
+            ServerUser user = users.FirstOrDefault(x => x.ID == id);
+            if(user != null)
+            {
+                users.Remove(user);
+                SendMessage(user.Name + " покинул чат!", 0);
+            }
+        }
+
+        public void SendMessage(string message, int id)
+        {
+            foreach(var item in users)
+            {
+                string answer = DateTime.Now.ToShortTimeString();
+                ServerUser user = users.FirstOrDefault(x => x.ID == id);
+                if (user != null)
+                {
+                    answer += ": " + user.Name;
+                }
+                answer += message;
+                item.operationContext.GetCallbackChannel<IServerChatCallback>().MessageCallbeck(answer);
+            }
+        }
+    }
+}
